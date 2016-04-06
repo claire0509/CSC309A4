93d968f2d56ba59a318f5e02b8c58620002d6b36
cvsxmw <cvsxmw@gmail.com>
2016-04-06 16:32:44 -0400

    Add locust testing file

---
+++ resApp/locust/locustfile.py
@@ -0,0 +1,68 @@
+from locust import HttpLocust, TaskSet, task
+
+import resouce
+
+# user credentials
+username = 'abc'
+password = '123'
+
+# user auth
+arg_u_a = 'abc'
+arg_u_b = 'abc'
+arg_u_c = 'hello'
+arg_u_d = 'world'
+arg_u_e = 'test'
+
+# post desc
+arg_p_a = 'Test location'
+arg_p_b = 'Test description'
+arg_p_c = '100'
+arg_p_d = '10'
+arg_p_e = '10'
+arg_p_f = '1000'
+arg_p_g = 'April 1 2016'
+
+
+class UserTask(TaskSet):
+
+	def on_start(l):
+		l.client.post("/auth/signup", {
+			"username": arg_u_a,
+			"password": arg_u_b,
+			"fname": arg_u_c,
+			"lname": arg_u_d,
+			"email": arg_u_e
+		})
+	
+	@task(1)
+	def login(l):
+		l.client.post("/auth/login", {
+			"username": arg_u_a, 
+			"password": arg_u_b})
+
+	@task(1)
+	def index(l):
+		l.client.get("/")
+
+	@task(1)
+	def posts(l):
+		l.client.post("/api/posts", {
+			"location": arg_p_a, 
+			"description": arg_p_b, 
+			"tcommute": arg_p_c, 
+			"nroom": arg_p_d, 
+			"nbathroom": arg_p_e, 
+			"price": arg_p_f, 
+			"created_by": arg_p_g})
+		 
+
+class APITask(HttpLocust):
+
+	resource.setrlimit(resource.RLIMIT_NOFILE, (999999, 999999))
+
+	task_set = UserTask
+	min_wait = 5 * 1000
+	max_wait = 15 * 1000
+	# website address
+	host = "http://localhost:3000"
+
